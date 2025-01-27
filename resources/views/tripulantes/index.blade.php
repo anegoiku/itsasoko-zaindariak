914ea13e0087631d1c8e99956654a0baab119def
IsardVDI <jstanton@fpzornotza.com>
2025-01-27 08:40:58 +0100

    azkena

--- resources/views/tripulantes/index.blade.php
+++ resources/views/tripulantes/index.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-    <title>Document</title>
+    <title>Tripulazioa</title>
 </head>
 
 <body>
